efc18955e445f8de7734722967b3e74e37071eb9
manirul41 <manirul41@gmail.com>
2018-11-20 17:16:27 +0600

    home architecture done.

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(TemplateView):
+    template_name = "home/index.html"
